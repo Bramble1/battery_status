0eafbb671ed5d77025aa95a4a1f19f69b120afb1
Sam <24442697+digitalXmage@users.noreply.github.com>
2022-03-02 16:13:16 +0000

    Add files via upload

---
+++ src/interface/interface.h
@@ -0,0 +1,8 @@
+#include "core/core.h"
+
+/*Interface.h relies upon and uses the functions in core folder
+ * Interface.* is used as the name implies and to make main look cleaner,everything done in here
+ * would be written in main()*/
+
+void process_user_input (int *argc,char *argv[],double *minutes);
+void init_battery_monitor(double *minutes);
