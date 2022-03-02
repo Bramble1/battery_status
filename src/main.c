ba40957878d3a24082d37f95e7c920e64e90504d
Sam <24442697+digitalXmage@users.noreply.github.com>
2022-03-02 16:08:35 +0000

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include "interface/interface.h"
+
+
+/*Main relies upon and uses the functions within interface/.h To increase readability*/
+int main(int argc, char *argv[])
+{
+	/*if no arguments, then print the help text and exit.*/
+/*	if(argc==1)
+	{
+		printf("./program <interval in minutes>\n");
+		exit(EXIT_SUCCESS);
+	} */
+
+	/*8 bytes of static memory set to 0 will hold interval time for battery status updates*/
+	double number = 0x0;
+	
+	/*obtain,sanatize and convert the user string to a number*/
+	process_user_input(&argc,argv,&number);
+
+	/*show battery status every interval (double number)*/
+	init_battery_monitor(&number);
+
+	return 0x0;
+}
