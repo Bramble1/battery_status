282ffe1b99c76b1be15c4e24fbeca9156450d59c
Sam D <24442697+Bramble1@users.noreply.github.com>
2023-10-17 09:14:38 +0100

    Update compile.sh
    Math library wasn't being incorporated properly due to the position of the "-lm" option.

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -W -g -lm -o sbat src/main.c src/interface/interface.c src/interface/interface.h src/interface/core/core.c src/interface/core/core.h   
+gcc -W -g -o sbat src/main.c src/interface/interface.c src/interface/interface.h src/interface/core/core.c src/interface/core/core.h -lm   
